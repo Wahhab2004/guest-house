c64b93a32246f84e0710af2f7caaea6cd7e0e78a
wahhab <wahhabawaludin2004@mail.ugm.ac.id>
2026-02-01 19:18:58 +0700

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import Providers from "./provider";
 import LayoutClient from "./layoutClient";
-
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-});
 
 export default function RootLayout({
 	children,
@@ -21,13 +10,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
+			<body className={` antialiased`}>
 				<Providers>
-					<LayoutClient>
-						{children}
-					</LayoutClient>
+					<LayoutClient>{children}</LayoutClient>
 					<Toaster position="top-right" reverseOrder={false} />
 				</Providers>
 			</body>
---
+++ src/app/metadata.ts
@@ -0,0 +1,11 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Ummu Ryosuke Guesthouse",
+	description: "The place that make you comfortable",
+	icons: {
+		icon: "favicon.ico", // ← WAJIB pakai /
+		shortcut: "favicon.ico",
+		apple: "favicon.ico",
+	},
+};
